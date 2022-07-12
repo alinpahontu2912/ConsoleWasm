7916a99e7fbf004e848634d19cf56f76ea34b65f
Stefan Pahontu <t-spahontu@microsoft.com>
2022-07-12 10:11:59 +0300

    improved styling and deleted unused variable

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,22 +10,21 @@ class Test
     {
         HttpClient client = new HttpClient();
         client.DefaultRequestHeaders.Add("User-Agent", "my-app");
-        String url = "https://raw.githubusercontent.com/radekdoulik/WasmPerformanceMeasurements/main/measurements/jsonDataFiles.txt";
-        String main = "https://raw.githubusercontent.com/radekdoulik/WasmPerformanceMeasurements/main/";
+        string main = "https://raw.githubusercontent.com/radekdoulik/WasmPerformanceMeasurements/main/";
         HttpResponseMessage response = await client.GetAsync(main + "measurements/jsonDataFiles.txt");
         if (response.StatusCode != HttpStatusCode.OK)
         {
             throw new Exception("HTTP request failed with status code " + response.StatusCode + "and message " + response.ReasonPhrase);
         }
-        String text = await response.Content.ReadAsStringAsync();
+        var text = await response.Content.ReadAsStringAsync();
         var lines = text.Split("\n");
-        for (int i = 20; i < 30; i++)
+        for (int i = 0; i < lines.Length; i++)
         {
             string? fileUrl = lines[i];
             response = await client.GetAsync(main + fileUrl);
             if (response.StatusCode != HttpStatusCode.OK)
             {
-                throw new Exception("HTTP request failed with status code " + response.StatusCode + "and message " + response.ReasonPhrase + main + fileUrl); // main + fileUrl for debugging
+                throw new Exception("HTTP request failed with status code " + response.StatusCode + "and message " + response.ReasonPhrase);
             }
             JsonObject obj = JsonNode.Parse(response.Content.ReadAsStringAsync().Result).AsObject();
             Console.WriteLine(obj);
